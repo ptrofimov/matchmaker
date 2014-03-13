4b80fea371e4c2452a20ebde0498bbde342937fd
pt <petrofimov@yandex.ru>
2014-03-13 23:04:27 +0300

    first validation

--- src/ArraySchema/validate.php
+++ src/ArraySchema/validate.php
@@ -3,5 +3,14 @@ namespace ArraySchema;
 
 function validate(array $array, array $schema)
 {
+    foreach ($schema as $key => $value) {
+        if (!array_key_exists($key, $array)) {
+            return false;
+        }
+        if ($array[$key] !== $value) {
+            return false;
+        }
+    }
+
     return true;
 }
