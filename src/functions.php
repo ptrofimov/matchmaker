efab9b511c48c38b79f63f86cccf685bf433abc3
pt <petrofimov@yandex.ru>
2014-05-07 21:34:53 +0300

    namespace refactoring

--- src/functions.php
+++ src/functions.php
@@ -1,10 +1,20 @@
 <?php
 
 if (!function_exists('array_keys_valid')) {
+
+    /**
+     * Validates array's keys
+     *
+     * @param array $array
+     * @param array $schema
+     *
+     * @return mixed
+     */
     function array_keys_valid(array $array, array $schema)
     {
-        require_once('functions/array_keys_valid.php');
+        require_once('arrays/validation.php');
+        require_once('arrays/keys/valid.php');
 
-        return functions\array_keys_valid($array, $schema);
+        return \arrays\keys\valid($array, $schema);
     }
 }
